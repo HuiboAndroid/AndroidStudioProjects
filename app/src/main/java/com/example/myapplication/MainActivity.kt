a7bb712bd51fecd326d7704117919a0049b430e6
Huibo Wang <huibo@fb.com>
2022-08-01 22:39:30 -0700

    app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    var checked = false;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        System.out.println("onCreate")
+        if (savedInstanceState != null) {
+            with(savedInstanceState) {
+                // Restore value of members from saved state
+                checked = getBoolean(STATE_CHECKED)
+            }
+        }
+
+        setContentView(R.layout.activity_main);
+        val mCheckBox: CheckBox = findViewById(R.id.checkBox)
+        mCheckBox.setChecked(checked);
+        mCheckBox.setOnCheckedChangeListener{ _, isChecked -> if(isChecked) {
+           checked = true
+        }}
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        System.out.println("onSaveInstanceState")
+        // Save the user's current game state
+        outState?.run {
+            putBoolean(STATE_CHECKED, checked)
+        }
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(outState)
+    }
+
+    companion object {
+        val STATE_CHECKED = "checked"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        System.out.println("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        System.out.println("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        System.out.println("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        System.out.println("onStop")
+    }
+
+    override fun onDestroy() {
+        System.out.println("onDestroy")
+        super.onDestroy()
+    }
+
+}
